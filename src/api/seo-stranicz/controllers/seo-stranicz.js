d56cc3c78c37a33c50938aaec6ede91cfdc22eae
Egor Demeshko <egor.demeshko@gmail.com>
2023-06-26 10:37:45 +0300

    new data

---
+++ src/api/seo-stranicz/controllers/seo-stranicz.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * seo-stranicz controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::seo-stranicz.seo-stranicz');

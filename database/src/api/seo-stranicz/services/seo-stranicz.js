01357c22e4279abc85f6ac5a6400886a3baef229
Egor Demeshko <egor.demeshko@gmail.com>
2023-06-28 08:31:07 +0300

    database set up

---
+++ database/src/api/seo-stranicz/services/seo-stranicz.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * seo-stranicz service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::seo-stranicz.seo-stranicz');
